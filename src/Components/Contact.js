ef9b9feef588fc4802c422ef59ae55a60fa1d53a
Shikha Pandey <79976467+Shikha148@users.noreply.github.com>
2026-06-21 04:06:11 +0530

    Updated component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import SnackBar from "my-react-snackbar";
 
-const FORMSPARK_URL = "https://submit-form.com/CARCkqSD";
+const FORMSPARK_URL = "https://submit-form.com/rLbw6UPRi";
 
 function Contact() {
   const [message, setMessage] = useState("");
